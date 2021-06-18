288540453469cbfd4b2d2bcb94d8adff5bf53796
manishgupt12 <85456258+manishgupt12@users.noreply.github.com>
2021-06-19 03:21:30 +0530

    Add files via upload

---
+++ fix_nvtheat.cpp
@@ -0,0 +1,58 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+
+ # Contributing Author: Manish Gupta, Syracuse University 
+
+------------------------------------------------------------------------- */
+
+#include "fix_nvtheat.h"
+#include <cstring>
+#include "group.h"
+#include "modify.h"
+#include "error.h"
+#include <iostream>
+
+using namespace LAMMPS_NS;
+using namespace FixConst;
+
+/* ---------------------------------------------------------------------- */
+
+FixNVTheat::FixNVTheat(LAMMPS *lmp, int narg, char **arg) :
+  FixNHheat(lmp, narg, arg)
+{
+  if (!tstat_flag)
+    error->all(FLERR,"Temperature control must be used with fix nvt");
+  if (pstat_flag)
+    error->all(FLERR,"Pressure control can not be used with fix nvt");
+
+  // create a new compute temp style
+  // id = fix-ID + temp
+
+  int n = strlen(id) + 6;
+  id_temp = new char[n];
+  strcpy(id_temp,id);
+  strcat(id_temp,"_temp");
+
+  char **newarg = new char*[3];
+  newarg[0] = id_temp;
+  newarg[1] = group->names[igroup];
+  newarg[2] = (char *) "temp";
+  //std::cout << newarg << " nvtheat newarg is priniyin " <<std::endl;
+  //std::cout << newarg[0] << " id_temp " <<std::endl;
+  //std::cout << newarg[1] << " group " <<std::endl;
+  //std::cout << newarg[2] << " chart temp " <<std::endl;
+  
+
+  modify->add_compute(3,newarg);
+  delete [] newarg;
+  tcomputeflag = 1;
+}
